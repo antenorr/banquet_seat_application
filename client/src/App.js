a8decf5feae3ca6398def85b767063fbfbdad75e
antenorr <roudyantenor@gmail.com>
2018-09-10 03:52:12 -0400

    react and express handshake success

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
+import Players from './components/Players';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      seated: [],
+      waiting: []
+    }
+    this.getAllSeats = this.getAllSeats.bind(this);
+    this.deleteGuest = this.deleteGuest.bind(this);
+  }
+
+  componentDidMount() {
+    this.getAllSeats();
+  }
+
+  getAllSeats = () => {
+    fetch('/api/seats')
+    .then( (res) => res.json() )// this step turns it into a js object
+    .then( data => {
+      // console.log(data)
+      this.setState({ 
+        seated: data.seated,
+        waiting: data.waiting
+      })
+    } )
+  }
+
+  deleteGuest = (id, specificList) => {
+    fetch(`/api/seats/deleteguest/${id}/${specificList}`, { method: 'DELETE' })
+    .then( res => res.json() )
+    .then( info => { 
+      this.setState.specificList = info
+    })
+  }
+
+
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
+        <nav>
+          <div className="nav-wrapper">
+            <a href="http://www.google.com" className="brand-logo center">The Banquet Seater</a>
+          </div>
+        </nav>
         <h1>Im working now!I'm reacting!!</h1>
+        <h3>Seated Guest(s) - Total: {(this.state.seated).length}</h3>
+        <Players seatedStatus={this.state.seated} deleteGuest={this.deleteGuest} />
+        <h3>Waiting Guests(s) - Total: {(this.state.waiting).length}</h3>
+        <Players seatedStatus={this.state.waiting} deleteGuest={this.deleteGuest}/>
+       
+
 
       </div>
     );
---
+++ client/src/components/Players.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './players.css';
+
+
+const Players = props => {
+    const { seatedStatus, deleteGuest } = props;
+ 
+    return (
+
+ 
+            <div className="row">
+                <ul >
+                    {seatedStatus.map((seat, index) =>
+                        <li key={index} >
+                            <div >
+                                <div className="col s12 m3">
+                                    <div className="card">
+                                        <div className="card-image image">
+                                            <span className="card-title">Guest: {seat.id}</span>
+                                        <a className="btn-floating halfway-fab waves-effect waves-light red" onClick={() => deleteGuest(seat.id, "seated")  }><i className="material-icons">clear</i></a>
+                                        </div>
+                                        <div className="card-content">
+                                            <p>{seat.name}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    )}
+                </ul>
+            </div>
+
+
+ 
+   
+
+
+
+
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Players;
